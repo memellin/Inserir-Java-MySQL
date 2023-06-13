fc81254af592a4c976d3387928337ecf6b57b010
memellin <enocksm@outlook.com>
2023-06-13 19:45:00 -0300

    Inserir dados MYSQL e JAVA

---
+++ src/application/Program.java
@@ -0,0 +1,47 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import db.DB;
+
+public class Program {
+	public static void main(String[] argrs) {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		Connection conn = null;
+		PreparedStatement st = null;
+		try {
+			conn = DB.getConnection();
+			
+			st = conn.prepareStatement(
+				    "INSERT INTO seller " +
+				    "(Name, Email, BirthDate, BaseSalary, DepartmentId) " +
+				    "VALUES (?, ?, ?, ?, ?)"
+				);
+			st.setString(1, "Karl Towns");
+			st.setString(2, "karl@gmail.com");
+			st.setDate(3, new java.sql.Date(sdf.parse("22/05/1997").getTime()));
+			st.setDouble(4, 3000.0);
+			st.setInt(5, 4);
+			
+			int rowsAffected = st.executeUpdate();
+			
+			System.out.println("Done! rows affected : "+ rowsAffected);
+			
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		catch (ParseException e) {
+			e.printStackTrace();
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
+	}
+}
